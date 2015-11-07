8174c47b8fa627d16436efaafcde5add0d5599af
knolegsb <seon2you@hotmail.com>
2015-11-07 11:56:43 -0800

    start and added model

---
+++ MigratingFromRazor/Models/OrderFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MigratingFromRazor.Models
+{
+    public class OrderFormModel
+    {
+        public int ProductId { get; set; }
+        public int OrderQty { get; set; }
+        [Required]
+        public string OrderShipping { get; set; }
+        [Required(ErrorMessage ="You must provide an email address.")]
+        public string OrderEmail { get; set; }
+    }
+}
