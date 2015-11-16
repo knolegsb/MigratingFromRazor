2ec0d6d83e413253891d26b63e5c8b5be6bf7d30
knolegsb <seon2you@hotmail.com>
2015-11-15 16:11:55 -0800

    set up database in web.config

--- MigratingFromRazor/Models/IdentityModels.cs
+++ MigratingFromRazor/Models/IdentityModels.cs
@@ -20,8 +20,8 @@ namespace MigratingFromRazor.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        //public ApplicationDbContext() : base("BakeryContext")
         {
         }
 
